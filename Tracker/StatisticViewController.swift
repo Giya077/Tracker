e9e6645a3247a941fda9d47d91c979aa32ebe8ab
Giya077 <giyaDevIos@gmail.com>
2024-05-08 14:17:35 +0500

    Сделал LaunchScreen и TabBar

---
+++ Tracker/StatisticViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StatisticViewController.swift
+//  Tracker
+//
+//  Created by GiyaDev on 05.05.2024.
+//
+
+import Foundation
+import UIKit
+
+final class StatisticViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
